7df062b94118a3ed6610e90972b21e3339ce7e97
Milhan <milhanfort@gmail.com>
2026-02-17 11:00:42 +0100

    test

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    let correctPassword = "vivitropcool";
+    let passInput = document.querySelector("#password");
+    let submitButton = document.querySelector("#submitBtn");
+
+    submitButton.addEventListener("click", checkPassword);
+    function checkPassword() {
+        let userInput = passInput.value;
+        if (userInput === correctPassword) {
+            document.getElementById("content").style.display = "block";
+        } else {
+            alert("Mot de passe incorrect. Essayez encore.");
+        }
+    }
+});
