6bbea4f0224fc592688f25ad2fe8133b845ec97d
Milhan <milhanfort@gmail.com>
2026-02-17 16:53:57 +0100

    fix

--- api/check-password.js
+++ api/check-password.js
@@ -1,5 +1,18 @@
-module.exports = function handler(req, res) {
-  const { password } = req.body;
+module.exports = async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+
+  let body = "";
+
+  await new Promise((resolve) => {
+    req.on("data", chunk => {
+      body += chunk;
+    });
+    req.on("end", resolve);
+  });
+
+  const { password } = JSON.parse(body);
 
   if (password === process.env.SITE_PASSWORD) {
     res.status(200).json({ success: true });
